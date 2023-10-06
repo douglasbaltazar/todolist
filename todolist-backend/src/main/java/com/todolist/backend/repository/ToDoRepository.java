d60067f4c199fa1750d81d673abe8e6ceaec19c3
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-10-06 00:06:30 -0300

    feat: backend first version

---
+++ todolist-backend/src/main/java/com/todolist/backend/repository/ToDoRepository.java
@@ -0,0 +1,11 @@
+package com.todolist.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.todolist.backend.model.ToDo;
+
+public interface ToDoRepository extends JpaRepository<ToDo, String> {
+	Optional<ToDo> findByName(String name);
+}
