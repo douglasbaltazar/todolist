b305665b1eddfb4c03d7b00c0830557faf70c4e9
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-10-06 00:47:31 -0300

    feat: frontend start

---
+++ todolist-frontend/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+
+export interface PeriodicElement {
+  name: string;
+  id: number;
+  value: number;
+  limitDate: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {id: 1, name: 'Hydrogen', value: 1.0079, limitDate: 'H'},
+  {id: 2, name: 'Helium', value: 4.0026, limitDate: 'He'},
+  {id: 3, name: 'Lithium', value: 6.941, limitDate: 'Li'},
+  {id: 4, name: 'Beryllium', value: 9.0122, limitDate: 'Be'},
+  {id: 5, name: 'Boron', value: 10.811, limitDate: 'B'},
+  {id: 6, name: 'Carbon', value: 12.0107, limitDate: 'C'},
+  {id: 7, name: 'Nitrogen', value: 14.0067, limitDate: 'N'},
+  {id: 8, name: 'Oxygen', value: 15.9994, limitDate: 'O'},
+  {id: 9, name: 'Fluorine', value: 18.9984, limitDate: 'F'},
+  {id: 10, name: 'Neon', value: 20.1797, limitDate: 'Ne'},
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ToDoList - FrontEnd';
+  displayedColumns: string[] = ['id', 'name', 'value', 'limitDate'];
+  dataSource = ELEMENT_DATA;
+}
---
+++ todolist-frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
