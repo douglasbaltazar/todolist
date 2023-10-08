1d0c620cbf9cc33dd6640c2d60ecf116142ae1e3
Douglas Baltazar <douglasbaltazar1@gmail.com>
2023-10-08 10:04:12 -0300

    feat: configuration and dialogs

--- todolist-backend/src/main/java/com/todolist/backend/model/Task.java
+++ todolist-backend/src/main/java/com/todolist/backend/model/Task.java
@@ -2,7 +2,6 @@ package com.todolist.backend.model;
 
 
 import java.time.LocalDateTime;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
